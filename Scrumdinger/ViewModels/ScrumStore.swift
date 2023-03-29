7a977becb2a8f1d0a214af85e8557421b9375146
Joao Gripp <corguinha@gmail.com>
2023-03-29 16:01:19 -0300

    Persisting Data - Create a data store

---
+++ Scrumdinger/ViewModels/ScrumStore.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Joao Gripp on 29/03/23.
+//
+
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+        
+    }
+}
